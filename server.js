51063645ca00b075242120d287dd07fe192fb7a3
Federico Pellegrini <fpellegrini@buenosaires.gob.ar>
2019-07-24 01:30:52 -0300

    installed compression middleware for expressjs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const app = express();
+
+app.use(compression());
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/got-houses'));
